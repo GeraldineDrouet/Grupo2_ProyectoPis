654faca31dedebfc5ae80fe8d1f4a538561f3e7b
Geraldine Drouet <gbdrouet@itb.edu.ec>
2022-12-16 20:22:56 -0500

    Login con firebase listo

--- app/src/main/java/com/example/elperlanegra/LoginActivity.java
+++ app/src/main/java/com/example/elperlanegra/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.elperlanegra;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,10 +23,14 @@ public class LoginActivity extends AppCompatActivity {
     EditText correo, contrasena;
     TextView regEnLog;
 
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
 
         logBtn = findViewById(R.id.buttonLog);
         regEnLog = findViewById(R.id.tv_regLog);
@@ -37,12 +49,46 @@ public class LoginActivity extends AppCompatActivity {
         logBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loginUser();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
     }
 
-    public void registro(View view) {
-        startActivity(new Intent(LoginActivity.this, RegistroActivity.class));
+    private void loginUser() {
+        String userCorreo = correo.getText().toString();
+        String userContrasena = contrasena.getText().toString();
+
+
+        //correo
+        if (TextUtils.isEmpty(userCorreo)){
+            Toast.makeText(this, "¡Campo <Correo Electrónico> está vacío!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //contraseña
+        if (TextUtils.isEmpty(userContrasena)){
+            Toast.makeText(this, "¡Campo <Contraseña> está vacío!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (userContrasena.length() < 6){
+            Toast.makeText(this, "¡¡ATENCIÓN!!\n"+"¡La contraseña debe tener más de 6 dígitos/caracteres!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //INICIANDO SESIÓN
+        auth.signInWithEmailAndPassword(userCorreo, userContrasena)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "¡INICIO DE SESIÓN EXITOSO!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Error: "+ task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 }
